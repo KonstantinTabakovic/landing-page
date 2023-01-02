9eb32d0436ab9c7109bec1998cd7210447a53199
KonstantinTabakovic <58007025+KonstantinTabakovic@users.noreply.github.com>
2023-01-03 00:08:58 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -44,6 +44,17 @@ window.addEventListener('scroll', function() {
   }
 });
 
+const navbar = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+
 
 
 
